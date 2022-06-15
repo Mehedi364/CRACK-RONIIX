bbbc79f3ffb9a1a0b880b67f405cdb88a36a5fad
Temporary Closed <106374961+YounisJohn@users.noreply.github.com>
2022-06-15 22:04:10 +0500

    Update Roniix.py

--- Roniix.py
+++ Roniix.py
@@ -10,7 +10,10 @@ if bit == '64bit':
     print("\n     \x1b[1;97m\x1b[1;41m Powerd By Younis John\x1b[0m") 
     print("")
     print("\n\x1b[1;92m Congratulations Your Device Support This Tool\033[1;37m")
-    print("\n\x1b[1;93m Please First Subscribe My YouTube Channel For Top Technology Tips & Tricks Thanku 🙂❤️\033[1;37m")
+    print("\n\x1b[1;93m Please First Subscribe My YouTube Channel For Top Technology Tips & Tricks Thanku 🙂\033[1;37m")
+    print("")
+    print("\n\x1b[1;97m Program Is Loading Please Wait.....") 
+    print("")
     os.system('xdg-open https://youtu.be/civNDQUwP2U/');time.sleep(6)
     import Ronixx
     Ronixx.Menu()
