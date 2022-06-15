73a82fa5171bb1e59fe1e9fc5cacbb9f18797d12
Temporary Closed <106374961+YounisJohn@users.noreply.github.com>
2022-06-15 21:49:38 +0500

    Create Roniix.py

---
+++ Roniix.py
@@ -0,0 +1,14 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations Your Device Support This Tool\033[1;37m")
+    print("\n\x1b[1;93m First Subscribe My YouTube Channel\033[1;37m")
+   # os.system('xdg-open https://youtu.be/civNDQUwP2U/');time.sleep(3)
+    import Ronixx
+    Ronixx.Menu()
