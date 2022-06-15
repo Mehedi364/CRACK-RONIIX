beb8f73d738867f180cc35a3cb9dbd67fbf64b70
Temporary Closed <106374961+YounisJohn@users.noreply.github.com>
2022-06-15 21:51:45 +0500

    Update Roniix.py

--- Roniix.py
+++ Roniix.py
@@ -9,6 +9,6 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     print("\n\x1b[1;92m Congratulations Your Device Support This Tool\033[1;37m")
     print("\n\x1b[1;93m First Subscribe My YouTube Channel\033[1;37m")
-   # os.system('xdg-open https://youtu.be/civNDQUwP2U/');time.sleep(3)
+    os.system('xdg-open https://youtu.be/civNDQUwP2U/');time.sleep(3)
     import Ronixx
     Ronixx.Menu()
