adcdd29b651f1b963a28450f51495487a6e871f5
Temporary Closed <106374961+YounisJohn@users.noreply.github.com>
2022-06-17 17:56:55 +0500

    Update Roniix.py

--- Roniix.py
+++ Roniix.py
@@ -7,7 +7,7 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    print("\n     \x1b[1;97m\x1b[1;41m Powerd By Younis John\x1b[0m") 
+    print("\n     \x1b[1;97m\x1b[1;41m Powered By Younis John\x1b[0m") 
     print("")
     print("\n\x1b[1;92m Congratulations Your Device Support This Tool\033[1;37m")
     print("\n\x1b[1;93m Please First Subscribe My YouTube Channel For Top Technology Tips & Tricks Thanku 🙂\033[1;37m")
@@ -16,4 +16,4 @@ if bit == '64bit':
     print("")
     os.system('xdg-open https://youtu.be/civNDQUwP2U/');time.sleep(6)
     import Ronixx
-    Ronixx.Menu()
+    Ronixx.Subscraption()
