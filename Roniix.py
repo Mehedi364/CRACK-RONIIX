aa4d7d642430c080c7436749931465b3a8bfe5cc
Temporary Closed <106374961+YounisJohn@users.noreply.github.com>
2022-06-17 18:25:05 +0500

    Update Roniix.py

--- Roniix.py
+++ Roniix.py
@@ -7,13 +7,13 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    print("\n     \x1b[1;97m\x1b[1;41m Powered By Younis John\x1b[0m") 
+    print("\n     \x1b[1;97m\x1b[1;41m Powered By Younis John \x1b[0m") 
     print("")
     print("\n\x1b[1;92m Congratulations Your Device Support This Tool\033[1;37m")
     print("\n\x1b[1;93m Please First Subscribe My YouTube Channel For Top Technology Tips & Tricks Thanku 🙂\033[1;37m")
     print("")
     print("\n\x1b[1;97m Program Is Loading Please Wait.....") 
     print("")
-    os.system('xdg-open https://youtu.be/civNDQUwP2U/');time.sleep(6)
+    os.system('xdg-open https://www.youtube.com/channel/UCfgDKN-UcG-19uyMBeNgX2Q/');time.sleep(6)
     import Ronixx
     Ronixx.Subscraption()
